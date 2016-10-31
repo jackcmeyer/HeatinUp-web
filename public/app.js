bcdfa92200e8f698fc7d861597e071a490099886
Anthony <tuffant21@aim.com>
2016-10-31 01:20:17 -0500

    Got the google map to show. Got the data points to show on the map. Seems to be working, now just gotta get the rest of the stuff in.

--- public/app.js
+++ public/app.js
@@ -1,19 +1,28 @@
 (function() {
     'use strict';
 
-    angular.module("heatin_up", ['ui.router'])
+    angular.module("heatin_up", ['ui.router', 'uiGmapgoogle-maps'])
         .config(config);
 
-    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+    config.$inject = ['$stateProvider', '$urlRouterProvider', 'uiGmapGoogleMapApiProvider'];
 
-    function config($stateProvider, $urlRouterProvider) {
+    function config($stateProvider, $urlRouterProvider, uiGmapGoogleMapApiProvider) {
+        // Google maps api stuff
+        uiGmapGoogleMapApiProvider.configure({
+            key: 'AIzaSyCgzf0nLxiHmIgNIQBJnvQjWZgtPqQaobk',
+            v: '3', //defaults to latest 3.X anyhow
+            libraries: 'visualization'
+        });
+
         // For any unmatched url, redirect to /state1
         $urlRouterProvider.otherwise('/login');
 
         $stateProvider
             .state('home', {
                 url: '/home',
-                templateUrl: 'partials/home.html'
+                templateUrl: 'partials/home.html',
+                controller: 'homeController',
+                controllerAs: 'vm'
             })
             .state('login', {
                 url: '/login',
